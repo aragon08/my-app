97d4abb70c0356daa9e8d3420d7010eb0493b11c
Carlos Aragon <aragon.c08@gmail.com>
2023-12-20 15:44:03 -0600

    commit inicial

---
+++ src/test/java/com/jccompany/app/MyCases.java
@@ -0,0 +1,32 @@
+package com.jccompany.app;
+
+import org.testng.annotations.Test;
+
+public class MyCases {
+
+    @Test
+    public void testOne() {
+        System.out.println("This is my first test");
+    }
+
+    @Test
+    public void testTwo() {
+        System.out.println("This is my second test");
+    }
+
+    @Test
+    public void testThree() {
+        System.out.println("This is my third test");
+    }
+
+    @Test
+    public void testFour() {
+        System.out.println("This is my fourth test");
+    }
+
+    @Test
+    public void testFive() {
+        System.out.println("This is my fifth test");
+    }
+
+}
---
+++ src/test/java/com/jccompany/app/MyCasesNew.java
@@ -0,0 +1,30 @@
+package com.jccompany.app;
+
+import org.testng.annotations.Test;
+
+public class MyCasesNew {
+    @Test
+    public void testSix() {
+        System.out.println("This is my sixth test");
+    }
+
+    @Test
+    public void testSeven() {
+        System.out.println("This is my seventh test");
+    }
+
+    @Test
+    public void testEight() {
+        System.out.println("This is my eighth test");
+    }
+
+    @Test
+    public void testNine() {
+        System.out.println("This is my ninth test");
+    }
+
+    @Test
+    public void testTen() {
+        System.out.println("This is my tenth test");
+    }
+}
